fff394140b70575efc352aacd2b32bcd4b852603
Sean Hellum <seanhellum45@gmail.com>
2020-07-13 18:38:15 +0000

    Fix bug where the program would crash if the file was a directory

--- files.go
+++ files.go
@@ -10,15 +10,17 @@ func GetFilesInDir(path string) ([]string, []string) {
 	var paths []string
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
-		PrintError("Path does not exist.", 127)
+		PrintError(err.Error(), 127)
 	}
 	for _, file := range files {
-		fileContents, err := ioutil.ReadFile(path + "/" + file.Name())
-		if err != nil {
-			PrintError("Could not read file.", 1)
+		if !file.IsDir() {
+			fileContents, err := ioutil.ReadFile(path + "/" + file.Name())
+			if err != nil {
+				PrintError(err.Error(), 1)
+			}
+			contents = append(contents, string(fileContents))
+			paths = append(paths, file.Name())
 		}
-		contents = append(contents, string(fileContents))
-		paths = append(paths, file.Name())
 	}
 	return contents, paths
 }
